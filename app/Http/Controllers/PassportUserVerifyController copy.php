fe7b7af60dad7e98a5282eea1edf7dcf66a2257e
Michael Aboff <mwaboff@gmail.com>
2020-11-17 20:43:48 -0500

    corrected code to determine who the user is when signed in, though now it appears that the logout button doesn't clear the cookie anymore

--- app/Http/Controllers/PassportUserVerifyController copy.php
+++ app/Http/Controllers/PassportUserVerifyController copy.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+
 use App;
-use \Laravel\Passport\PassportUserProvider;
 use \League\OAuth2\Server\ResourceServer;
 use \Laravel\Passport\TokenRepository;
 use \Laravel\Passport\Guards\TokenGuard;
 use \Laravel\Passport\ClientRepository;
 use \Illuminate\Support\Facades\Auth;
+use \Laravel\Passport\PassportUserProvider;
+use \Laravel\Passport\PassportServiceProvider;
 use \Illuminate\Http\Request;
 
-class PassportUserVerifyController {
 
-  static function getUser($bearerToken) {
+
+class PassportUserVerifyController extends Controller 
+{
+
+  // Thank you to infostreams! https://gist.github.com/infostreams/1b827a688c76250e7acb7626906469a8
+  public static function getUser($bearerToken) {
     $result_user = null;
     try {
       $tokenguard = new TokenGuard(
@@ -23,22 +30,26 @@ class PassportUserVerifyController {
         App::make(ClientRepository::class), 
         App::make('encrypter')
       );
+
+      dd($tokenguard);
+  
       $request = Request::create('/');
       $request->headers->set('Authorization', $bearerToken);
-      $result_user = $tokenguard->user($request);
+      Auth::setRequest($request);
+      $result_user = Auth::user();
     } catch (Exception $e) {
       dd($e);
     }
-
+    
     return $result_user;
   }
-  
-  static function authorizeUser($bearerToken) {
+
+  public static function authorizeUser($bearerToken) {
     $request = request();
-    $request->headers->set('Authorization', $bearerToken);
+    $request->headers->set('Authorization', 'Bearer ' . $bearerToken);
     Auth::setRequest($request);
   
     return Auth::user();
   }
-  
+
 }
