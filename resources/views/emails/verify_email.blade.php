2764715c78da116089232a02f2e7352157afad9f
Michael Aboff <mwaboff@gmail.com>
2021-01-03 03:38:10 -0500

    corrected spelling error in email to send to customer for email verification

--- resources/views/emails/verify_email.blade.php
+++ resources/views/emails/verify_email.blade.php
@@ -5,7 +5,7 @@
   </head>
   <body>
     <h2>MonkeyScripts Email Verification</h2>
-    <p>Welcome to the MonkeyScripts community, {{ $username }}! Before you will be able to log in, we will have to verify your email addreacss. Please do so by clicking the below link:</p>
+    <p>Welcome to the MonkeyScripts community, {{ $username }}! Before you will be able to log in, we will have to verify your email address. Please do so by clicking the below link:</p>
     <a href="{{ $activation_link }}" target="_blank">Click Here To Verify Email</a>
   </body>
 </html>
