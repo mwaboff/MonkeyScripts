9b458fbab4464df6da75b0afeb88bd5ee4b72360
Michael Aboff <mwaboff@gmail.com>
2021-01-02 21:40:04 -0500

    login will not succeed without your account being verified working

--- app/Http/Middleware/ApiLogin.php
+++ app/Http/Middleware/ApiLogin.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Illuminate\Support\Facades\DB;
 
+use App\User;
+
 use Closure;
 
 class ApiLogin
@@ -26,9 +28,25 @@ class ApiLogin
             'client_id' => 1,
             'client_secret' => $secret
         ]);
+
+        // Check to make sure user's email is verified
+
+        $user = User::where('email', $request['username'])->first();
+        if(isset($user)) {
+            if (isset($user->email_verified_at)) {
+                return $next($request);
+            } else {
+                dd(json_encode([
+                    'success' => false,
+                    'message' => 'Email needs to be verified before logging in.'
+                ]));
+            }
+        }
+
+        dd(json_encode([
+            'success' => false,
+            'message' => 'Account not found.'
+        ]));
         
-        
-        
-        return $next($request);
     }
 }
